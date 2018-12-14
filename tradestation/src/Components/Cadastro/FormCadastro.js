e5419a2352f7dec97bac2a84ae724ed56ca978fd
Pedro Henrique Costa Maia 115111114 <pedrohcm@lcc3-100.lcc.ufcg.edu.br>
2018-12-14 11:25:58 -0300

    adiouuuuuuu aefoiuhasiopefioajsf

--- tradestation/src/Components/Cadastro/FormCadastro.js
+++ tradestation/src/Components/Cadastro/FormCadastro.js
@@ -1,35 +1,67 @@
 import React from 'react'
 import { Button, Form } from 'semantic-ui-react'
 
-const FormCadastro = () => (
-    <Form>
-        <Form.Field required>
-            <label>Nome</label>
-            <input placeholder='Digite um nome...' />
-        </Form.Field>
-        <Form.Group>
-            <Form.Field required width={8}>
-                <label>Usuário</label>
-                <input placeholder='Digite seu nome de usuário...' />
-            </Form.Field>
-            <Form.Field required width={8}>
-                <label>E-mail</label>
-                <input placeholder='Digite seu e-mail...' />
-            </Form.Field>
-        </Form.Group>
-        <Form.Group>
-            <Form.Field required width={8}>
-                <label>Digite sua senha</label>
-                <input placeholder='Digite sua senha...' />
-            </Form.Field>
-            <Form.Field required width={8}>
-                <label>Confirme sua senha</label>
-                <input placeholder='Digite novamente a senha...' />
-            </Form.Field>
-        </Form.Group>
+class FormAnuncio extends Component {
 
-        <Button basic fluid color='red' type='submit'>Cadastrar</Button>
-    </Form>
-)
+    nomeChange = event => {
+        this.setState({ nome: event.target.value });
+    }
+    usuarioChange = event => {
+        this.setState({ valor: event.target.value });
+    }
+    emailChange = event => {
+        this.setState({ descricao: event.target.value });
+    }
+    senhaChange = event => {
+        this.setState({ imagem: event.target.value });
+    }
 
-export default FormCadastro
+    handleSubmit = event => {
+        event.preventDefault();
+
+        const usuario = {
+            "nome": this.state.nome,
+            "usuario": +this.state.usuario,
+            "email": this.state.email,
+            "senha": this.state.senha,
+        };
+        axios.post("http://localhost:3001/usuario/", anuncio);
+        this.props.close();
+        window.location.reload();
+    }
+
+    //FALTA COLOCAR OS ONCHANGE NO INPUT
+
+
+    render() {
+        return (
+            <Form>
+                <Form.Field required>
+                    <label>Nome</label>
+                    <input placeholder='Digite um nome...' />
+                </Form.Field>
+                <Form.Group>
+                    <Form.Field required width={8}>
+                        <label>Usuário</label>
+                        <input placeholder='Digite seu nome de usuário...' />
+                    </Form.Field>
+                    <Form.Field required width={8}>
+                        <label>E-mail</label>
+                        <input placeholder='Digite seu e-mail...' />
+                    </Form.Field>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Field required width={8}>
+                        <label>Digite sua senha</label>
+                        <input placeholder='Digite sua senha...' />
+                    </Form.Field>
+                    <Form.Field required width={8}>
+                        <label>Confirme sua senha</label>
+                        <input placeholder='Digite novamente a senha...' />
+                    </Form.Field>
+                </Form.Group>
+
+                <Button basic fluid color='red' type='submit'>Cadastrar</Button>
+            </Form>
+        )
+    }
--- tradestation/src/Components/Home/InfoAnuncio.js
+++ tradestation/src/Components/Home/InfoAnuncio.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Container, Header } from 'semantic-ui-react'
+import { Container, Header, Segment, Image, Grid, Divider, Table, Icon } from 'semantic-ui-react'
 import FormCadastro from '../Cadastro/FormCadastro'
 
 export default class InfoAnuncio extends Component {
@@ -9,7 +9,67 @@ export default class InfoAnuncio extends Component {
         <Header as='h3' textAlign='center'>
           Cadastro
             </Header>
-        <FormCadastro></FormCadastro>
+        <Segment>
+          <Grid columns={2} relaxed='very'>
+            <Grid.Column>
+              <p>
+                <Image src='https://react.semantic-ui.com/images/wireframe/short-paragraph.png' />
+              </p>
+              <p>
+                <Image src='https://react.semantic-ui.com/images/wireframe/short-paragraph.png' />
+              </p>
+              <p>
+                <Image src='https://react.semantic-ui.com/images/wireframe/short-paragraph.png' />
+              </p>
+              <p>
+                <Image src='https://react.semantic-ui.com/images/wireframe/short-paragraph.png' />
+              </p>
+            </Grid.Column>
+            <Grid.Column>
+              <Divider horizontal>
+                <Header as='h4'>
+                  <Icon name='tag' />
+                  Description
+      </Header>
+              </Divider>
+
+              <p>
+                Doggie treats are good for all times of the year. Proven to be eaten by 99.9% of all dogs
+                worldwide.
+    </p>
+
+              <Divider horizontal>
+                <Header as='h4'>
+                  <Icon name='bar chart' />
+                  Specifications
+      </Header>
+              </Divider>
+
+              <Table definition>
+                <Table.Body>
+                  <Table.Row>
+                    <Table.Cell width={2}>Size</Table.Cell>
+                    <Table.Cell>1" x 2"</Table.Cell>
+                  </Table.Row>
+                  <Table.Row>
+                    <Table.Cell>Weight</Table.Cell>
+                    <Table.Cell>6 ounces</Table.Cell>
+                  </Table.Row>
+                  <Table.Row>
+                    <Table.Cell>Color</Table.Cell>
+                    <Table.Cell>Yellowish</Table.Cell>
+                  </Table.Row>
+                  <Table.Row>
+                    <Table.Cell>Odor</Table.Cell>
+                    <Table.Cell>Not Much Usually</Table.Cell>
+                  </Table.Row>
+                </Table.Body>
+              </Table>
+            </Grid.Column>
+          </Grid>
+
+          <Divider vertical>And</Divider>
+        </Segment>
       </Container>
     )
   }
